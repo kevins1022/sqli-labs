9de19aa3055896b332c0e603bbc3246ea9de1c21
kevins1022 <ningyuqiao456@gmail.com>
2016-05-11 23:22:04 +0800

    双引号字符型注入(POST)

--- Less-14/index.php
+++ Less-14/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Less-14- Double Injection- Double quotes- String</title>
+	<title>Less-14-双引号字符型注入(POST)</title>
 </head>
 
 <body bgcolor="#000000">
